01af3bd7e1ec192d6f603769abf87989f81e8050
Regimar <60851078+regimarnegreiros@users.noreply.github.com>
2025-08-15 21:04:31 -0300

    Atividade 4

---
+++ atividade04/Grafo.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Grafo(ABC):
+    @abstractmethod
+    def numero_de_vertices(self):
+        """Retorna o número de vértices."""
+        pass
+
+    @abstractmethod
+    def numero_de_arestas(self):
+        """Retorna o número de arestas."""
+        pass
+
+    @abstractmethod
+    def sequencia_de_graus(self):
+        """Retorna a sequência de graus."""
+        pass
+
+    @abstractmethod
+    def adicionar_aresta(self, u, v):
+        """Adiciona uma nova aresta."""
+        pass
+
+    @abstractmethod
+    def remover_aresta(self, u, v):
+        """Remove uma aresta."""
+        pass
+
+    @abstractmethod
+    def imprimir(self):
+        """Imprime a matriz / lista ligada do grafo."""
+        pass
---
+++ atividade04/GrafoDenso.py
@@ -0,0 +1,48 @@
+from Grafo import Grafo
+
+class GrafoDenso(Grafo):
+    def __init__(self, rotulos: int | list):
+        if isinstance(rotulos, int):
+            self.rotulos = list(range(rotulos))
+        elif isinstance(rotulos, list):
+            self.rotulos = rotulos
+        else:
+            raise ValueError("Rotulo inválido.")
+        
+        self.n = len(self.rotulos)
+        self.matriz = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        self.arestas = 0
+
+    def numero_de_vertices(self):
+        return self.n
+
+    def numero_de_arestas(self):
+        return self.arestas
+
+    def sequencia_de_graus(self):
+        graus = [sum(linha) for linha in self.matriz]
+        return graus
+
+    def adicionar_aresta(self, u, v):
+        i = self.rotulos.index(u)
+        j = self.rotulos.index(v)
+
+        if self.matriz[i][j] == 0:
+            self.matriz[i][j] = 1
+            self.matriz[j][i] = 1
+            self.arestas += 1
+
+    def remover_aresta(self, u, v):
+        i = self.rotulos.index(u)
+        j = self.rotulos.index(v)
+
+        if self.matriz[i][j] == 1:
+            self.matriz[i][j] = 0
+            self.matriz[j][i] = 0
+            self.arestas -= 1
+
+    def imprimir(self):
+        print("Matriz de adjacência: ")
+        print(" ", " ".join(map(str, self.rotulos)))
+        for i in range(self.n):
+            print(self.rotulos[i], " ".join(map(str, self.matriz[i])))
